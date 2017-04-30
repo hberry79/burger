2fc3bd4b7cfb79c005d4eb0e062679cde643857b
Heather Berry <heatherberry@Heathers-MacBook-Air.local>
2017-04-30 18:43:13 -0400

    setting up environment

---
+++ config/orm.js
@@ -0,0 +1,16 @@
+var connection = require("../config/connection.js");
+
+var orm={
+	selectAll: function(){
+
+	},
+	insertOne: function(){
+
+	},
+	updateOne: function(){
+
+	}
+}//end of orm object
+
+// Export the orm object 
+module.exports = orm;
---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+ /*In this file, write insert queries to populate the burgers table with at least three entries.*/
+ USE burgers_db;
+
+ INSERT INTO burgers (burger_name) VALUES ('Mushroom swiss');
+ INSERT INTO burgers (burger_name) VALUES ('Siracha Cheddar');
+ INSERT INTO burgers (burger_name) VALUES ('Tuna & Arugula');
+ INSERT INTO burgers (burger_name) VALUES ('Double Bacon');
+ INSERT INTO burgers (burger_name) VALUES ('Black Bean Patty');
